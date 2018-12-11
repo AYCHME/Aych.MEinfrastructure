e053d58462bdb2c1b36a517c72b718aa976b879f
pranksteess <xhxpecer@gmail.com>
2018-12-11 15:27:50 +0800

    [修复]-异步调用合约代币列表

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -30,6 +30,8 @@
 #include <fc/variant.hpp>
 #include <signal.h>
 #include <cstdlib>
+#include <fstream>
+#include <iostream>
 
 namespace eosio {
 
@@ -1424,6 +1426,12 @@ string read_only::get_all_token_contracts(const read_only::get_all_token_contrac
          }
       }
       result += "\n";
+   }
+   if (p.file != "") {
+         std::ofstream outfile;
+         outfile.open(p.file);
+         outfile  << result;
+         outfile.close();
    }
    return result;
 }
