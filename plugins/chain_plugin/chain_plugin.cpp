240e07dbefa987d329950cff13327945b6f405a6
specerxi <xhxpecer@126.com>
2018-12-29 01:42:38 +0800

    [修改]- 抵押给别人的值

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -1363,6 +1363,7 @@ string read_only::get_eos_holders(const read_only::get_eos_holders_params& param
             delegate_to_others += a;
          }
 
+         delegate_to_others -= self_delegate_bw; // 减去自身抵押的，就是给别人抵押的总值
          string s_tmp = delegate_to_others.to_string();
          string total = s_tmp.substr(0, s_tmp.find_first_of(" "));
          result += "," + total;
